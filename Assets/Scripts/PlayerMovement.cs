43404640e6babbbeed4f661f77271bcb0f69606a
Black Drift <fovet.vianney@gmail.com>
2024-04-09 13:05:21 +0200

    updating PlayerMovement (again)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -34,7 +34,6 @@ public class PlayerMovement : MonoBehaviour
             animator.SetBool("isWalking", true);
 
             rb.Move(direction * speed * Time.deltaTime);
-
         }
         else
         {
