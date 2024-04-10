7f92422351e01de75411d7cf0fe09638f8edd46f
Chotaki <116464680+Chotaki@users.noreply.github.com>
2024-04-11 01:58:07 +0200

    frog rotation & door opening on correct rotation

--- Assets/Scripts/LastPuzzle.cs
+++ Assets/Scripts/LastPuzzle.cs
@@ -5,23 +5,58 @@ using UnityEngine;
 
 public class LastEnigma : MonoBehaviour
 {
-    public Transform FrogStatue;
+
+    IEnumerator RotateFrog(Vector3 byAngles, float inTime)
+    {
+        var fromAngle = transform.rotation;
+        var toAngle = Quaternion.Euler(transform.eulerAngles + byAngles);
+        for (var t = 0f; t < 1; t+= Time.deltaTime/inTime)
+        {
+            transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+            yield return null;
+        }
+    }
+
+    /*public Transform FrogStatue;
     public float speed = 1.0f;
     public void rotateStatue()
     {
-        //Vector3 targetDirection = new Vector3 (transform.eulerAngles.x, transform.eulerAngles.y + 90, transform.eulerAngles.z);
+        *//*//Vector3 targetDirection = new Vector3 (transform.eulerAngles.x, transform.eulerAngles.y + 90, transform.eulerAngles.z);
         Vector3 targetDirection = FrogStatue.position - transform.position;
         float singleStep = speed * Time.deltaTime;
         Vector3 newDirection = Vector3.RotateTowards(transform.eulerAngles, targetDirection, singleStep, 0.0f);
 
         if (Input.GetKeyDown(KeyCode.F))
         {
-            /*newDirection = Vector3.RotateTowards(transform.eulerAngles, rotation, 90, 90);*/
+            *//*newDirection = Vector3.RotateTowards(transform.eulerAngles, rotation, 90, 90);*//*
             transform.rotation = Quaternion.LookRotation(newDirection);
-        }
-    }
+        }*//*
 
-    /* void Update()
+    }*/
+
+    /* 
+     * 	IEnumerator RotateMe(Vector3 byAngles, float inTime) 
+	    {	var fromAngle = transform.rotation;
+		    var toAngle = Quaternion.Euler(transform.eulerAngles + byAngles);
+		    for(var t = 0f; t < 1; t += Time.deltaTime/inTime) {
+			    transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+			    yield return null;
+		    }
+	    }
+        void Update () 
+        {
+	        if(Input.GetKeyDown("e")){
+	        StartCoroutine(RotateMe(Vector3.up * 90, 0.8f));
+	        }
+	        if(Input.GetKeyDown("q")){
+	        StartCoroutine(RotateMe(Vector3.up * -90, 0.8f));
+	        }
+          }
+        }
+     * 
+     * 
+     * 
+     * void Update()
     {
         // Determine which direction to rotate towards
         Vector3 targetDirection = target.position - transform.position;
@@ -39,8 +74,36 @@ public class LastEnigma : MonoBehaviour
         transform.rotation = Quaternion.LookRotation(newDirection);
     }*/
 
+    public Transform frog;
+    public Transform door;
+    public Vector3 correctRotation;
+    public bool doorOpening = false;
+
+    public void openDoor()
+    {
+        if (doorOpening)
+        {
+            door.Rotate(0, 90, 0);
+            doorOpening = false;
+        }
+    }
+
     void Update()
     {
-        rotateStatue();
+        if (Input.GetKeyDown(KeyCode.F))
+        { 
+            StartCoroutine(RotateFrog(Vector3.up * 90, 0.8f));
+            //frog.Rotate(0, 90, 0);
+            //transform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+
+        Debug.Log(frog.eulerAngles.y);
+        Debug.Log(correctRotation.y + 1);
+
+        if (frog.eulerAngles.y < correctRotation.y + 1 && frog.eulerAngles.y > correctRotation.y - 1)
+        {
+            doorOpening = true;
+            openDoor();
+        }
     }
 }
