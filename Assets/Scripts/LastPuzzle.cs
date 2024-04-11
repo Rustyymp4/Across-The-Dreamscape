481073e224929c3fd56440b22911cea952209b2e
Chotaki <116464680+Chotaki@users.noreply.github.com>
2024-04-11 13:05:23 +0200

    door opening correction

--- Assets/Scripts/LastPuzzle.cs
+++ Assets/Scripts/LastPuzzle.cs
@@ -5,105 +5,60 @@ using UnityEngine;
 
 public class LastEnigma : MonoBehaviour
 {
-
-    IEnumerator RotateFrog(Vector3 byAngles, float inTime)
-    {
-        var fromAngle = transform.rotation;
-        var toAngle = Quaternion.Euler(transform.eulerAngles + byAngles);
-        for (var t = 0f; t < 1; t+= Time.deltaTime/inTime)
-        {
-            transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
-            yield return null;
-        }
-    }
-
-    /*public Transform FrogStatue;
-    public float speed = 1.0f;
-    public void rotateStatue()
-    {
-        *//*//Vector3 targetDirection = new Vector3 (transform.eulerAngles.x, transform.eulerAngles.y + 90, transform.eulerAngles.z);
-        Vector3 targetDirection = FrogStatue.position - transform.position;
-        float singleStep = speed * Time.deltaTime;
-        Vector3 newDirection = Vector3.RotateTowards(transform.eulerAngles, targetDirection, singleStep, 0.0f);
-
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            *//*newDirection = Vector3.RotateTowards(transform.eulerAngles, rotation, 90, 90);*//*
-            transform.rotation = Quaternion.LookRotation(newDirection);
-        }*//*
-
-    }*/
-
-    /* 
-     * 	IEnumerator RotateMe(Vector3 byAngles, float inTime) 
-	    {	var fromAngle = transform.rotation;
-		    var toAngle = Quaternion.Euler(transform.eulerAngles + byAngles);
-		    for(var t = 0f; t < 1; t += Time.deltaTime/inTime) {
-			    transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
-			    yield return null;
-		    }
-	    }
-        void Update () 
-        {
-	        if(Input.GetKeyDown("e")){
-	        StartCoroutine(RotateMe(Vector3.up * 90, 0.8f));
-	        }
-	        if(Input.GetKeyDown("q")){
-	        StartCoroutine(RotateMe(Vector3.up * -90, 0.8f));
-	        }
-          }
-        }
-     * 
-     * 
-     * 
-     * void Update()
-    {
-        // Determine which direction to rotate towards
-        Vector3 targetDirection = target.position - transform.position;
-
-        // The step size is equal to speed times frame time.
-        float singleStep = speed * Time.deltaTime;
-
-        // Rotate the forward vector towards the target direction by one step
-        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
-
-        // Draw a ray pointing at our target in
-        Debug.DrawRay(transform.position, newDirection, Color.red);
-
-        // Calculate a rotation a step closer to the target and applies rotation to this object
-        transform.rotation = Quaternion.LookRotation(newDirection);
-    }*/
-
     public Transform frog;
     public Transform door;
     public Vector3 correctRotation;
     public bool doorOpening = false;
+    public bool doorIsOpen = false;
+    public bool frogRotating = false; 
 
-    public void openDoor()
+    IEnumerator RotateFrog(Vector3 byAngles, float inTime)
+    {
+        var fromAngle = frog.rotation;
+        var toAngle = Quaternion.Euler(frog.eulerAngles + byAngles);
+        for (var t = 0f; t < 1; t+= Time.deltaTime/inTime)
+        {
+            frog.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+            yield return null;
+        }
+        frogRotating = false;
+    }
+
+    IEnumerator OpenDoor(Vector3 byAngles, float inTime)
     {
         if (doorOpening)
         {
-            door.Rotate(0, 90, 0);
-            doorOpening = false;
+            var fromAngle = door.rotation;
+            var toAngle = Quaternion.Euler(door.eulerAngles + byAngles);
+            for (var t = 0f; t < 1; t += Time.deltaTime / inTime)
+            {
+                door.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+                yield return null;
+            }
         }
+        doorOpening = false;
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.F))
         { 
-            StartCoroutine(RotateFrog(Vector3.up * 90, 0.8f));
-            //frog.Rotate(0, 90, 0);
-            //transform.rotation = Quaternion.Euler(0, 90, 0);
+            if (!frogRotating)
+            {
+                frogRotating = true;
+                StartCoroutine(RotateFrog(Vector3.up * 90, 0.8f));
+            }
         }
 
-        Debug.Log(frog.eulerAngles.y);
-        Debug.Log(correctRotation.y + 1);
+        /*Debug.Log(frog.eulerAngles.y);
+        Debug.Log(correctRotation.y + 1);*/
 
-        if (frog.eulerAngles.y < correctRotation.y + 1 && frog.eulerAngles.y > correctRotation.y - 1)
+        if (frog.eulerAngles.y < correctRotation.y + 1 && frog.eulerAngles.y > correctRotation.y - 1 && doorIsOpen == false)
         {
             doorOpening = true;
-            openDoor();
+            StartCoroutine(OpenDoor(Vector3.up * 90, 0.8f));
+            doorIsOpen = true;
+            //StopCoroutine(OpenDoor(Vector3.up * 90, 0.8f));
         }
     }
 }
